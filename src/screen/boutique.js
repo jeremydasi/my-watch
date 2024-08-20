c7d7ed122955c8bd22801cf84b7c31744a28f7d0
Jérémy DASI <jeremy.dasi@gmail.com>
2024-08-20 18:12:15 +0200

    img importé et title centré

--- src/screen/boutique.js
+++ src/screen/boutique.js
@@ -1,9 +1,21 @@
 import '../style/boutique.css'
+import festinaHommeBleu from '../assets/festina-montre-homme-bleu.svg'
+import festinaHommeClassique from '../assets/festina-montre-homme-classique.svg'
+import festinaFemmeRose from'../assets/festina-montre-femme-rose.svg'
+import festinaFemmeDiamant from '../assets/festina-montre-femme-diamant.svg'
+import tommyHommeBleu from '../assets/Tommy-montre-homme-bleu.svg'
+import tommyHommeNoir from '../assets/Tommy-montre-noir-homme.svg'
+import tommyFemmeRose from '../assets/Tommy-montre-rose-femme.svg'
+import tommyFemmeOr from '../assets/Tommy-montre-or-femme.svg'
+import bossHommeBleu from '../assets/boss-montre-homme.svg'
+import bossHommeNoir from '../assets/boss-montre-noir-homme.svg'
+import bossFemmeRose from '../assets/boss-montre-rose-femme.svg'
+import bossFemmeOr from'../assets/boss-montre-or-femme.svg'
 
 function Boutique() {
     return(
         <div>
-            <h1>Boutique</h1>
+            <h1 className='title'>Boutique</h1>
         </div>
     )
 }
