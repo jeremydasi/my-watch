7bec1393881311cc0f1c125da1d1499537973401
Jérémy DASI <jeremy.dasi@gmail.com>
2024-08-21 16:58:00 +0200

    Rajout d'une montre par ligne

--- src/screen/boutique.js
+++ src/screen/boutique.js
@@ -1,14 +1,17 @@
 import '../style/boutique.css'
 import festinaHommeBleu from '../assets/festina-montre-homme-bleu.svg'
 import festinaHommeClassique from '../assets/festina-montre-homme-classique.svg'
+import festinaHommeOrNoir from '../assets/festina-montre-homme-or-bleu.svg'
 import festinaFemmeRose from'../assets/festina-montre-femme-rose.svg'
 import festinaFemmeDiamant from '../assets/festina-montre-femme-diamant.svg'
 import tommyHommeBleu from '../assets/Tommy-montre-homme-bleu.svg'
 import tommyHommeNoir from '../assets/Tommy-montre-noir-homme.svg'
 import tommyFemmeRose from '../assets/Tommy-montre-rose-femme.svg'
 import tommyFemmeOr from '../assets/Tommy-montre-or-femme.svg'
+import tommyFemmeArgent from '../assets/Tommy-montre-argent-femme.svg'
 import bossHommeBleu from '../assets/boss-montre-homme.svg'
 import bossHommeNoir from '../assets/boss-montre-noir-homme.svg'
+import bossHommeCuivreNoir from '../assets/boss-montre-or-noir-homme.svg'
 import bossFemmeBlanc from '../assets/boss-montre-rose-femme.svg'
 import bossFemmeOr from'../assets/boss-montre-or-femme.svg'
 
@@ -27,6 +30,10 @@ function Boutique() {
                 <div className='image'>
                     <img src={festinaHommeClassique} alt='montre festina classique' />
                     <p className='descriptionMontre'>Montre Festina pour homme Classique Bleu 79 €</p>
+                </div>
+                <div className='image'>
+                    <img src={festinaHommeOrNoir} alt='montre festina or et bleu' />
+                    <p className='descriptionMontre'>Montre Festina pour homme Chrono Sport Bleu 179 €</p>
                 </div>
                 <div className='image'>
                     <img src={festinaFemmeRose} alt='montre festina rose' />
@@ -55,6 +62,10 @@ function Boutique() {
                     <img src={tommyFemmeOr} alt='montre tommy hilfiger or' />
                     <p className='descriptionMontre'>Montre Tommy Hilfiger Or pour femme 169 €</p>
                 </div>
+                <div className='image'>
+                    <img src={tommyFemmeArgent} alt='montre tommy hilfiger argent' />
+                    <p className='descriptionMontre'>Montre Tommy Hilfiger Ella Argenté pour femme 129 €</p>
+                </div>
             </div>
 
             <div className='containerImg'>
@@ -65,6 +76,10 @@ function Boutique() {
                 <div className='image'>
                     <img src={bossHommeNoir} alt='montre hugo boss classique' />
                     <p className='descriptionMontre'>Montre Hugo Boss Champion Noir pour homme 479 €</p>
+                </div>
+                <div className='image'>
+                    <img src={bossHommeCuivreNoir} alt='montre hugo boss cuivre et noir' />
+                    <p className='descriptionMontre'>Montre Hugo Boss Champion cuivre et Noir pour homme 479 €</p>
                 </div>
                 <div className='image'>
                     <img src={bossFemmeBlanc} alt='montre hugo boss blanc' />
